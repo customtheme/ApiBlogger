33fdc4916ae321650691d6b93608a00326fadd30
Ani Sunaryati <32156532+customtheme@users.noreply.github.com>
2018-06-24 09:23:21 +0700

    Create cse_ramaibeud.js

---
+++ cse_ramaibeud.js
@@ -0,0 +1,9 @@
+(function() {
+    var cx = '012875795175539201205:0xg6rbdtkjk';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
