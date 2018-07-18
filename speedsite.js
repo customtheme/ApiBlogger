d4f867f3d956271d421df9a9c81a21f669895cd6
Ani Sunaryati <32156532+customtheme@users.noreply.github.com>
2018-07-19 00:21:30 +0700

    Create speedsite.js

---
+++ speedsite.js
@@ -0,0 +1,9 @@
+$(document).ready(function()
+{
+var aa=$("#copyright-content").val();
+if (aa == null) {
+    window.location.href = "https://ramaibeud.blogspot.com/";
+};
+ $("#copyright-content").attr("href","https://ramaibeud.blogspot.com/");
+
+});
